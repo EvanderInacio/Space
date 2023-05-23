474a16e6dbd23038e1a9d1dd40f9c1b1ced623b3
Evander Inacio <evander.20112@hotmail.com>
2023-05-23 19:38:29 -0300

    Feat: Routes :truck:

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,28 @@
-import fastify from "fastify";
-import { PrismaClient } from "@prisma/client"
+import 'dotenv/config'
+
+import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true,
+})
+app.register(jwt, {
+  secret: 'spacetime',
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('🚀 HTTP server running on http://localhost:3333')
-})
+app.register(authRoutes)
+app.register(memoriesRoutes)
+
+
+app
+  .listen({
+    port: 3333
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on port http://localhost:3333')
+  })

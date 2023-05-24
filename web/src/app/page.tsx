5e8600236ef1c96f20695fc4d2a269dde731cfa9
Evander Inacio <evander.20112@hotmail.com>
2023-05-23 21:22:59 -0300

    Feat: Page Profile :zap:

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,14 @@
+import { cookies } from 'next/headers'
+
 import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
 import { SignIn } from '@/components/Signin'
+import { Profile } from '@/components/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid grid-cols-2 min-h-screen ">
       <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
@@ -11,7 +16,7 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2  rounded-full blur-full bg-purple-700 opacity-50" />
         <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes " />
         
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero />
         <Copyright />
       </div>

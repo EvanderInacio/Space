57e26d3457554838391db5286495c1e0a469ab44
Evander Inacio <evander.20112@hotmail.com>
2023-05-25 22:32:59 -0300

    Feat: Finished Server :fire:

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,22 @@
 import 'dotenv/config'
 
 import fastify from 'fastify'
-import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { memoriesRoutes } from './routes/memories'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -17,12 +27,11 @@ app.register(jwt, {
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
-
+app.register(uploadRoutes)
 
 app
   .listen({
-    port: 3333
+    port: 3333,
+    host: '0.0.0.0',
   })
-  .then(() => {
-    console.log('🚀 HTTP server running on port http://localhost:3333')
-  })
+  .then(() => console.log('Server is running!'))
